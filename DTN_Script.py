80bd04b155a6eba59c67e9fa7421f1729a0a6da8
Mitchell Moore <mmoo97@uab.edu>
2020-11-12 11:46:40 -0600

    Fix width and height

--- DTN_Script.py
+++ DTN_Script.py
@@ -11,67 +11,67 @@ source_endpoint_id_list = ["ff3045ea-a112-11ea-8f07-0a21f750d19b"]
 source_dirs = "/globus/datasets/ds01/"
 dest_dir = "/data/user/mmoo97/TEST_TRANSFER/"
 
-''' Code to get access token'''
-client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
-client.oauth2_start_flow(refresh_tokens=True)
-
-authorize_url = client.oauth2_get_authorize_url()
-print('Please go to this URL and login: {0}'.format(authorize_url))
-
-# this is to work on Python2 and Python3 -- you can just use raw_input() or
-# input() for your specific version
-get_input = getattr(__builtins__, 'raw_input', input)
-auth_code = get_input(
-    'Please enter the code you get after login here: ').strip()
-token_response = client.oauth2_exchange_code_for_tokens(auth_code)
-
-globus_auth_data = token_response.by_resource_server['auth.globus.org']
-globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
-# the refresh token and access token, often abbr. as RT and AT
-transfer_rt = globus_transfer_data['refresh_token']
-transfer_at = globus_transfer_data['access_token']
-expires_at_s = globus_transfer_data['expires_at_seconds']
-
-# Now we've got the data we need, but what do we do?
-# That "GlobusAuthorizer" from before is about to come to the rescue
-authorizer = globus_sdk.RefreshTokenAuthorizer(
-    transfer_rt, client, access_token=transfer_at, expires_at=expires_at_s)
-
-tc = globus_sdk.TransferClient(authorizer=authorizer)
-
-# high level interface; provides iterators for list responses
-print("My Endpoints:")
-for ep in tc.endpoint_search(filter_scope="recently-used"):
-    print("[{}] {}".format(ep["id"], ep["display_name"]))
-
-
-for task in tc.task_list():
-    print("Task({}): {} -> {}".format(
-        task["task_id"], task["source_endpoint"],
-        task["destination_endpoint"]))
-  
-tc.update_task
-tdata = globus_sdk.TransferData(tc, source_endpoint_id_list[0],
-                                 destination_endpoint_id,
-                                 label="Dataset Test",
-                                sync_level=None, verify="checksum")
-# tdata.add_item("/globus/datasets/ds04/", "/data/user/mmoo97/TEST_TRANSFER/",
+# ''' Code to get access token'''
+# client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
+# client.oauth2_start_flow(refresh_tokens=True)
+#
+# authorize_url = client.oauth2_get_authorize_url()
+# print('Please go to this URL and login: {0}'.format(authorize_url))
+#
+# # this is to work on Python2 and Python3 -- you can just use raw_input() or
+# # input() for your specific version
+# get_input = getattr(__builtins__, 'raw_input', input)
+# auth_code = get_input(
+#     'Please enter the code you get after login here: ').strip()
+# token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+#
+# globus_auth_data = token_response.by_resource_server['auth.globus.org']
+# globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+# # the refresh token and access token, often abbr. as RT and AT
+# transfer_rt = globus_transfer_data['refresh_token']
+# transfer_at = globus_transfer_data['access_token']
+# expires_at_s = globus_transfer_data['expires_at_seconds']
+#
+# # Now we've got the data we need, but what do we do?
+# # That "GlobusAuthorizer" from before is about to come to the rescue
+# authorizer = globus_sdk.RefreshTokenAuthorizer(
+#     transfer_rt, client, access_token=transfer_at, expires_at=expires_at_s)
+#
+# tc = globus_sdk.TransferClient(authorizer=authorizer)
+#
+# # high level interface; provides iterators for list responses
+# print("My Endpoints:")
+# for ep in tc.endpoint_search(filter_scope="recently-used"):
+#     print("[{}] {}".format(ep["id"], ep["display_name"]))
+#
+#
+# for task in tc.task_list():
+#     print("Task({}): {} -> {}".format(
+#         task["task_id"], task["source_endpoint"],
+#         task["destination_endpoint"]))
+#
+# tdata = globus_sdk.TransferData(tc, source_endpoint_id_list[0],
+#                                  destination_endpoint_id,
+#                                  label="Dataset Test",
+#                                 sync_level=None, verify="checksum")
+# # tdata.add_item("/globus/datasets/ds04/", "/data/user/mmoo97/TEST_TRANSFER/",
+# #                 recursive=True)
+# # for item in tdata.items:
+# #     print(item)
+# tdata.add_item(source_dirs, dest_dir,
 #                 recursive=True)
-# for item in tdata.items:
-#     print(item)
-tdata.add_item(source_dirs, dest_dir,
-                recursive=True)
-transfer_result = tc.submit_transfer(tdata)
-print("task_id =", transfer_result["task_id"])
-
-# task = tc.get_task(transfer_result["task_id"])
-task = tc.get_task("750a002a-0901-11eb-abd6-0213fe609573")
-if task["status"] == "SUCCEEDED":
-    print("Task Status: " + task["status"])
-    print("Effective Speed: " + str(round(task["effective_bytes_per_second"]/(1024*1024), 2)) + " MB/s")
-else:
-    print("Task Status: " + task["status"])
-    print("MB Transferred: " + str(round(task["bytes_transferred"]/1000000, 2)) + " MB")
+# print(tdata.items())
+# # transfer_result = tc.submit_transfer(tdata)
+# # print("task_id =", transfer_result["task_id"])
+#
+# # task = tc.get_task(transfer_result["task_id"])
+# task = tc.get_task("750a002a-0901-11eb-abd6-0213fe609573")
+# if task["status"] == "SUCCEEDED":
+#     print("Task Status: " + task["status"])
+#     print("Effective Speed: " + str(round(task["effective_bytes_per_second"]/(1024*1024), 2)) + " MB/s")
+# else:
+#     print("Task Status: " + task["status"])
+#     print("MB Transferred: " + str(round(task["bytes_transferred"]/1000000, 2)) + " MB")
 
 # File parse
 ds_sizes = {'ds01': 0.1, 'ds04':10, 'ds06':100, 'ds08': 1000, 'ds10': 1000, 'ds12':100, 
@@ -136,8 +136,8 @@ x = np.arange(len(ds_names))  # the label locations
 width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots(2, 1)
-# fig.set_figheight(4)
-fig.figsize()
+fig.set_figheight(7)
+fig.set_figwidth(9)
 
 rects1 = ax[0].bar(x - width/2, src_to_oncampy, width, label='Source > OnCampus')
 rects2 = ax[0].bar(x + width/2, oncampy_to_src, width, label='OnCampus > Source')
