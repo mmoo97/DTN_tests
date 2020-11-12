9afa372e136bab86505f1901026e1e7af973f15b
Mitchell Moore <mmoo97@uab.edu>
2020-11-12 12:13:52 -0600

    Modularize script elements into separate files

---
+++ vars.py
@@ -0,0 +1,6 @@
+''' Access Variables '''
+CLIENT_ID = '7a71f6ff-14d4-4e3a-8dbf-0e23a573f519' # this can be obtained from Apps and Services -> Projects -> Client ID
+destination_endpoint_id = "9c8c88c2-ea4a-11e6-b9ba-22000b9a448b"
+source_endpoint_id_list = ["ff3045ea-a112-11ea-8f07-0a21f750d19b"]
+source_dirs = "/globus/datasets/ds01/"
+dest_dir = "/data/user/mmoo97/TEST_TRANSFER/"
