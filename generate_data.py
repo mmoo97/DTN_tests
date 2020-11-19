5da7df555bd865e1298b0dfa65f95ac855bfb7f7
Mitchell Moore <mmoo97@uab.edu>
2020-11-19 08:57:18 -0600

    reformat whitespace

--- generate_data.py
+++ generate_data.py
@@ -5,18 +5,17 @@ import vars
 client = globus_sdk.NativeAppAuthClient(vars.CLIENT_ID)
 client.oauth2_start_flow(refresh_tokens=True)
 
-authorize_url = client.oauth2_get_authorize_url()
-print('Please go to this URL and login: {0}'.format(authorize_url))
+print('Please go to this URL and login: {0}'
+      .format(client.oauth2_get_authorize_url()))
 
-# this is to work on Python2 and Python3 -- you can just use raw_input() or
-# input() for your specific version
 get_input = getattr(__builtins__, 'raw_input', input)
-auth_code = get_input(
-    'Please enter the code you get after login here: ').strip()
+auth_code = get_input('Please enter the code here: ').strip()
 token_response = client.oauth2_exchange_code_for_tokens(auth_code)
 
 globus_auth_data = token_response.by_resource_server['auth.globus.org']
+
 globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+
 # the refresh token and access token, often abbr. as RT and AT
 transfer_rt = globus_transfer_data['refresh_token']
 transfer_at = globus_transfer_data['access_token']
@@ -24,6 +23,7 @@ expires_at_s = globus_transfer_data['expires_at_seconds']
 
 # Now we've got the data we need, but what do we do?
 # That "GlobusAuthorizer" from before is about to come to the rescue
+
 authorizer = globus_sdk.RefreshTokenAuthorizer(
     transfer_rt, client, access_token=transfer_at, expires_at=expires_at_s)
 
@@ -34,7 +34,7 @@ print("My Endpoints:")
 for ep in tc.endpoint_search(filter_scope="recently-used"):
     print("[{}] {}".format(ep["id"], ep["display_name"]))
 
-
+print("\n")
 for task in tc.task_list():
     print("Task({}): {} -> {}".format(
         task["task_id"], task["source_endpoint"],
